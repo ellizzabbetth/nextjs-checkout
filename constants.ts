073423ddb90de535c73d436c542d18088c5a4266
elizabeth <ellizzabbetth@gmail.com>
2023-02-13 09:22:49 -0500

    cleanup

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export default {
     endpoint: 'http://localhost:8000/api/checkout',
-    stripe_key: 'sk_test_51LsPzyH418K4ULt8MOn3qF8sAEqr1O8BcQJz5bJxAb3gfcclwG7k3ePN7C7WyaJNY7HRL2ZkFF9x9ELUaPpNz3RH00OdULBca1'
+    stripe_key: process.env.STRIPE_API_KEY
 }
