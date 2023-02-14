791c51a5c9ba553a90d7e9fef973da7c4e923060
elizabeth <ellizzabbetth@gmail.com>
2023-02-14 07:34:57 -0500

    footer

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import Head from "next/head";
+import Footer from "./Footer";
 
 const Wrapper = (props) => {
     return (
@@ -14,6 +15,7 @@ const Wrapper = (props) => {
             <div className="container">
                 {props.children}
             </div>
+            {/* <Footer/> */}
         </div>
     );
 };
--- pages/[code].tsx
+++ pages/[code].tsx
@@ -230,11 +230,21 @@ const Home = ({user1, product1, order1, quantity1}: Props) => {
                             </div>
                         </div>
                         <button className="btn btn-primary btn-lg btn-block" type="submit">Checkout</button>
+
                     </form>
+                    <footer className="my-5 pt-5 text-muted text-center text-small">
+                        <p className="mb-1">&copy; 2017-2018 Company Name</p>
+                        <ul className="list-inline">
+                        <li className="list-inline-item"><a href="#">Privacy</a></li>
+                        <li className="list-inline-item"><a href="#">Terms</a></li>
+                        <li className="list-inline-item"><a href="#">Support</a></li>
+                        </ul>
+                    </footer>
                 </div>
             </div>
         </Wrapper>
     )
+    
 };
 
 // @ts-ignore
